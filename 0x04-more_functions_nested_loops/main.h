008b8ef46c723145597728694a121a5611df6984
abdeslam388 <abdeslamelamrani38@gmail.com>
2023-03-23 13:19:46 -0700

    first commit

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,8 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-#include <stdio.h>
-
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
@@ -15,5 +12,4 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
-
 #endif
