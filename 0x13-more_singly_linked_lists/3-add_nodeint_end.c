1569d2c5298ebf5c494de39c752b39d181fa81a9
abdeslam388 <abdeslamelamrani38@gmail.com>
2023-04-29 16:02:02 -0700

    test blank line

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -10,6 +10,7 @@
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new_node = malloc(sizeof(listint_t));
+
 	if (new_node == NULL)
 	{
 
@@ -18,6 +19,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 
 	new_node->n = n;
 	new_node->next = NULL;
+
 	if (*head == NULL)
 	{
 		*head = new_node;
@@ -25,6 +27,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	else
 	{
 		listint_t *last_node = *head;
+
 		while (last_node->next != NULL)
 		{
 			last_node = last_node->next;
